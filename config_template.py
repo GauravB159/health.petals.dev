772f1f69fdaa226af920b03645577c727238ba43
Gaurav Bhagchandani <gaurav@supportinglines.com>
2023-09-01 06:10:38 -0400

    Added template

---
+++ config_template.py
@@ -0,0 +1,12 @@
+from data_structures import ModelInfo
+
+
+INITIAL_PEERS = [~INITIAL_PEERS~]
+
+MODELS = [
+    ModelInfo(
+        dht_prefix="CodeLlama-34b-Instruct-hf",
+        repository="https://huggingface.co/codellama/CodeLlama-34b-Instruct-hf",
+        num_blocks=48,
+    )
+]
