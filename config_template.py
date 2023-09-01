a76048af873aba1b59d3e176713549d3e1c0b88d
Gaurav Bhagchandani <gaurav@supportinglines.com>
2023-09-01 12:35:33 -0400

    Health template

--- config_template.py
+++ config_template.py
@@ -8,5 +8,10 @@ MODELS = [
         dht_prefix="CodeLlama-34b-Instruct-hf",
         repository="https://huggingface.co/codellama/CodeLlama-34b-Instruct-hf",
         num_blocks=48,
-    )
+    ),
+    ModelInfo(
+        dht_prefix="StableBeluga2-hf",
+        repository="https://huggingface.co/petals-team/StableBeluga2",
+        num_blocks=80,
+    ),
 ]
